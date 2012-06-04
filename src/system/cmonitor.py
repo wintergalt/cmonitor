f697e1171369aa5bb665e93e97e3abfc0cc78fac
wintergalt <wintergalt@gmail.com>
2012-06-04 10:02:12 -0300

    Format changes

--- src/system/cmonitor.py
+++ src/system/cmonitor.py
@@ -9,54 +9,54 @@ import paramiko
 import interactive
 
 class CMonitorWorker():
-  def connect(self):
-    pass
+    def connect(self):
+        pass
 
-  def do_work(self):
-    pass
+    def do_work(self):
+        pass
 
 
 class ProcessMonitor(CMonitorWorker):
-  def connect(self):
-    try:
-      username = 'jboss'
-      hostname = 'srvdmz2'
-      port = 22
-      password = 'nolosabe'
-      self.client = paramiko.SSHClient()
+    def connect(self):
+        try:
+            username = 'jboss'
+            hostname = 'srvdmz2'
+            port = 22
+            password = 'nolosabe'
+            self.client = paramiko.SSHClient()
+            
+            self.client.load_system_host_keys()
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            if password == '':
+              # ask for one on stdin
+              password = getpass.getpass('Password for %s@%s: ' % (username, hostname))
+            
+            self.client.connect(hostname, port, username, password)
+            self.transport = self.client.get_transport()
+            self.chan = None
 
-      self.client.load_system_host_keys()
-      self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      if password == '':
-        # ask for one on stdin
-        password = getpass.getpass('Password for %s@%s: ' % (username, hostname))
-      
-      self.client.connect(hostname, port, username, password)
-      self.transport = self.client.get_transport()
-      self.chan = None
+        except Exception, e:
+            print '*** Caught exception: %s: %s' % (e.__class__, e)
+            traceback.print_exc()
+        try:
+          self.client.close()
+        except:
+          pass
 
-    except Exception, e:
-      print '*** Caught exception: %s: %s' % (e.__class__, e)
-      traceback.print_exc()
-      try:
-        self.client.close()
-      except:
-        pass
+    def do_work(self):
+        self.chan = self.transport.open_session()
+        self.chan.exec_command('ps -ef | grep httpd | wc -l')
+        result = self.chan.recv(1024)
+        return result
 
-  def do_work(self):
-    self.chan = self.transport.open_session()
-    self.chan.exec_command('ps -ef | grep httpd | wc -l')
-    result = self.chan.recv(1024)
-    return result
-
-  def close(self):
-    if self.chan:
-      self.chan.close()
-    if self.client:
-      self.client.close()
+    def close(self):
+        if self.chan:
+          self.chan.close()
+        if self.client:
+          self.client.close()
 
 if __name__ == '__main__':
-  pm = ProcessMonitor()
-  pm.connect()
-  print(pm.do_work())
-  pm.close()
+    pm = ProcessMonitor()
+    pm.connect()
+    print(pm.do_work())
+    pm.close()
--- src/system/tray.py
+++ src/system/tray.py
@@ -6,55 +6,55 @@ from cmonitor import *
 
 class SystemTrayIcon(QtGui.QSystemTrayIcon):
 
-  show_message = QtCore.Signal(str,str,int)
+    show_message = QtCore.Signal(str, str, int)
 
-  def __init__(self, icon, parent=None):
-    QtGui.QSystemTrayIcon.__init__(self, icon, parent)
-    self.menu = QtGui.QMenu(parent)
-    exitAction = self.menu.addAction('Exit')
-    exitAction.triggered.connect(self.exit)
-    self.setContextMenu(self.menu)
-    self.pm = ProcessMonitor()
-    self.timer = Timer(10, self)
-    self.timer.daemon = True
-    self.timer.start()
-    self.show_message.connect(self.message)
-    self.setIcon(QtGui.QIcon('process.png'))
-
-
-  def message(self, title, text, msecs):
-    self.showMessage(title,text, QtGui.QSystemTrayIcon.Critical, msecs=msecs)
+    def __init__(self, icon, parent=None):
+        QtGui.QSystemTrayIcon.__init__(self, icon, parent)
+        self.menu = QtGui.QMenu(parent)
+        exitAction = self.menu.addAction('Exit')
+        exitAction.triggered.connect(self.exit)
+        self.setContextMenu(self.menu)
+        self.pm = ProcessMonitor()
+        self.timer = Timer(10, self)
+        self.timer.daemon = True
+        self.timer.start()
+        self.show_message.connect(self.message)
+        self.setIcon(QtGui.QIcon('process.png'))
 
 
-  def exit(self):
-    self.pm.close()
-    sys.exit(0)
+    def message(self, title, text, msecs):
+        self.showMessage(title, text, QtGui.QSystemTrayIcon.Critical, msecs=msecs)
+
+
+    def exit(self):
+        self.pm.close()
+        sys.exit(0)
 
 
 class Timer(threading.Thread):
-  def __init__(self, seconds, tray_icon):
-    threading.Thread.__init__(self)
-    self.runTime = seconds
-    self.tray_icon = tray_icon
+    def __init__(self, seconds, tray_icon):
+        threading.Thread.__init__(self)
+        self.runTime = seconds
+        self.tray_icon = tray_icon
 
 
-  def run(self):
-    self.tray_icon.pm.connect()
-    while True:
-      time.sleep(self.runTime)
-      procs = self.tray_icon.pm.do_work()
-      self.tray_icon.setToolTip('Procesos: {0}'.format(procs))
-      if int(procs) > 30:
-        self.tray_icon.show_message.emit('Alerta procesos http',
-                                         'Actualmente {0}'.format(procs),
-                                         1000*(self.runTime-1))
+    def run(self):
+        self.tray_icon.pm.connect()
+        while True:
+          time.sleep(self.runTime)
+          procs = self.tray_icon.pm.do_work()
+          self.tray_icon.setToolTip('Procesos: {0}'.format(procs))
+          if int(procs) > 30:
+            self.tray_icon.show_message.emit('Alerta procesos http',
+                                             'Actualmente {0}'.format(procs),
+                                             1000 * (self.runTime - 1))
 
 
 if __name__ == '__main__':
-  t0 = time.clock()
-  app = QtGui.QApplication(sys.argv)
-  style = app.style()
-  icon = QtGui.QIcon(style.standardPixmap(QtGui.QStyle.SP_FileIcon))
-  trayIcon = SystemTrayIcon(icon)
-  trayIcon.show()
-  app.exec_()
+    t0 = time.clock()
+    app = QtGui.QApplication(sys.argv)
+    style = app.style()
+    icon = QtGui.QIcon(style.standardPixmap(QtGui.QStyle.SP_FileIcon))
+    trayIcon = SystemTrayIcon(icon)
+    trayIcon.show()
+    app.exec_()
